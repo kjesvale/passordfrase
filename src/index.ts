2c9ac9b52a88630a7e8b1959f1eecf4c99d53b52
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2020-09-18 23:09:39 +0200

    Legg til knapper for å hente nytt passord og for kopiering

--- src/index.ts
+++ src/index.ts
@@ -28,5 +28,20 @@ const kombinertSetning: Ordklasse[] = [
 
 const femTilfeldigeOrd: Ordklasse[] = Array(5).fill(Ordklasse.AlleOrd);
 
-const passordOutput = document.querySelector('output#passord');
+const passordOutput = document.querySelector('#passord');
+const nyttPassordKnapp = document.querySelector('#nytt-passord');
+const kopierPassordKnapp = document.querySelector('#kopier-passord');
+
 passordOutput.textContent = opprettPassord(alternativSetning);
+
+nyttPassordKnapp.addEventListener('click', (e) => {
+    passordOutput.textContent = opprettPassord(alternativSetning);
+});
+
+kopierPassordKnapp.addEventListener('click', (e) => {
+    try {
+        navigator.clipboard.writeText(passordOutput.textContent);
+    } catch (e) {
+        console.error('Kunne ikke kopiere passord til clipboard:', e);
+    }
+}
